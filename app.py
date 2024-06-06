d4cd7f7439786f7f40247c79ea9ad875aab7d7f8
k4my <kmabara0229@icloud.com>
2024-06-06 16:15:38 +0900

    update

--- app.py
+++ app.py
@@ -43,9 +43,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    msg = event.message
+    if msg.text == 'test':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='ok'))
 
 if __name__ == "__main__":
 #    app.run()
